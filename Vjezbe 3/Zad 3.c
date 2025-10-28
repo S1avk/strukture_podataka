d12e5763ad1c395964489b320cd8194de2e4f4ae
S1avk <99097278+S1avk@users.noreply.github.com>
2025-10-28 16:12:23 +0100

    Zadataj 3

---
+++ Vjezbe 3/Zad 3.c
@@ -0,0 +1,181 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_CHAR 10
+
+typedef struct osoba* pozicija;
+typedef struct osoba {
+    char ime[MAX_CHAR];
+    char prezime[MAX_CHAR];
+    int godina;
+    pozicija next;
+} osoba;
+
+int dodavanje(pozicija, char*, char*, int);
+int ispis(pozicija);
+int kraj(pozicija, char*, char*, int);
+int nadiprezime(pozicija, char*);
+int brisanje(pozicija, char*, char*, int);
+int dodajIza(pozicija, char*, char*, int, char*);
+int dodajIspred(pozicija, char*, char*, int, char*);
+int sortiraj(pozicija);
+int upisUDatoteku(pozicija, char*);
+int citajIzDatoteke(pozicija, char*);
+
+int main() {
+    osoba head;
+    strcpy(head.ime, ""); strcpy(head.prezime, ""); head.godina = 0; head.next = NULL;
+
+    dodavanje(&head, "Slavko", "Kozina", 2004);
+    kraj(&head, "Ante", "Pirija", 2004);
+    dodavanje(&head, "Nikola", "Bareta", 2004);
+
+    printf("Pocetna lista:\n");
+    ispis(head.next);
+
+    dodajIza(&head, "Marin", "Devalle", 2004, "Kozina");
+    dodajIspred(&head, "Bruno", "Kosta", 2005, "Pirija");
+
+    printf("\nLista nakon dodavanja:\n");
+    ispis(head.next);
+
+    sortiraj(&head);
+    printf("\nSortirana lista:\n");
+    ispis(head.next);
+
+    upisUDatoteku(head.next, "osobe.txt");
+
+    osoba head2;
+    strcpy(head2.ime, ""); strcpy(head2.prezime, ""); head2.godina = 0; head2.next = NULL;
+    citajIzDatoteke(&head2, "osobe.txt");
+
+    printf("\nLista ucitana iz datoteke:\n");
+    ispis(head2.next);
+
+    return 0;
+}
+
+int dodavanje(pozicija p, char* ime, char* prezime, int godina) {
+    pozicija q = (pozicija)malloc(sizeof(osoba));
+    if (!q) return -1;
+    strcpy(q->ime, ime);
+    strcpy(q->prezime, prezime);
+    q->godina = godina;
+    q->next = p->next;
+    p->next = q;
+    return 0;
+}
+
+int ispis(pozicija p) {
+    if (p == NULL) {
+        printf("Lista je prazna\n");
+        return 0;
+    }
+    while (p != NULL) {
+        printf("%s %s %d\n", p->ime, p->prezime, p->godina);
+        p = p->next;
+    }
+    return 0;
+}
+
+int kraj(pozicija p, char* ime, char* prezime, int godina) {
+    while (p->next != NULL) p = p->next;
+    dodavanje(p, ime, prezime, godina);
+    return 0;
+}
+
+int nadiprezime(pozicija p, char* prezime) {
+    p = p->next;
+    while (p != NULL) {
+        if (strcmp(p->prezime, prezime) == 0) {
+            printf("Pronadena osoba: %s %s\n", p->ime, p->prezime);
+            return 0;
+        }
+        p = p->next;
+    }
+    printf("Osoba nije pronadena\n");
+    return 0;
+}
+
+int brisanje(pozicija p, char* ime, char* prezime, int godina) {
+    pozicija temp;
+    while (p->next != NULL) {
+        temp = p->next;
+        if (strcmp(temp->ime, ime) == 0 && strcmp(temp->prezime, prezime) == 0 && temp->godina == godina) {
+            p->next = temp->next;
+            free(temp);
+            return 0;
+        }
+        p = p->next;
+    }
+    return 0;
+}
+
+// A. Dodavanje iza određenog elementa (po prezimenu)
+int dodajIza(pozicija p, char* ime, char* prezime, int godina, char* targetPrezime) {
+    pozicija q = p->next;
+    while (q != NULL && strcmp(q->prezime, targetPrezime) != 0)
+        q = q->next;
+    if (q == NULL) return -1;
+    dodavanje(q, ime, prezime, godina);
+    return 0;
+}
+
+// B. Dodavanje ispred određenog elementa (po prezimenu)
+int dodajIspred(pozicija p, char* ime, char* prezime, int godina, char* targetPrezime) {
+    while (p->next != NULL && strcmp(p->next->prezime, targetPrezime) != 0)
+        p = p->next;
+    if (p->next == NULL) return -1;
+    dodavanje(p, ime, prezime, godina);
+    return 0;
+}
+
+// C. Sortiranje po prezimenima (bubble sort po linkovima)
+int sortiraj(pozicija head) {
+    pozicija i, j, prev, temp;
+    int swapped;
+    do {
+        swapped = 0;
+        prev = head;
+        i = head->next;
+        while (i != NULL && i->next != NULL) {
+            j = i->next;
+            if (strcmp(i->prezime, j->prezime) > 0) {
+                i->next = j->next;
+                j->next = i;
+                prev->next = j;
+                swapped = 1;
+            }
+            prev = prev->next;
+            i = prev->next;
+        }
+    } while (swapped);
+    return 0;
+}
+
+// D. Upis u datoteku
+int upisUDatoteku(pozicija p, char* imeDatoteke) {
+    FILE* fp = fopen(imeDatoteke, "w");
+    if (!fp) return -1;
+    while (p != NULL) {
+        fprintf(fp, "%s %s %d\n", p->ime, p->prezime, p->godina);
+        p = p->next;
+    }
+    fclose(fp);
+    return 0;
+}
+
+// E. Citanje iz datoteke
+int citajIzDatoteke(pozicija head, char* imeDatoteke) {
+    FILE* fp = fopen(imeDatoteke, "r");
+    if (!fp) return -1;
+    char ime[MAX_CHAR], prezime[MAX_CHAR];
+    int godina;
+    while (fscanf(fp, "%s %s %d", ime, prezime, &godina) == 3) {
+        kraj(head, ime, prezime, godina);
+    }
+    fclose(fp);
+    return 0;
+}
